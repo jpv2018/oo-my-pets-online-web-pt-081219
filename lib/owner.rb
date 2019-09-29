391f1144deaa60a8cff6b0bbfbd8298d7536ec0e
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-29 22:30:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,15 @@
 class Owner
+  
   attr_reader :name
+  
+  @@all = []
+  
+  @@count = 0
   
   def initialize(name)
     @name = name
     @species = "human"
+    @@count += 1
   end
   
   def say_species
